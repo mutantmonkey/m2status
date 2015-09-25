6f93ff36f47fb927cfbf7b4e0c5d6dbacf81abcd
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-09-24 19:07:04 -0500

    clean up things a bit

--- bar.go
+++ bar.go
@@ -18,9 +18,7 @@ type BarWidget struct {
 	Status    string `json:"_status,omitempty"`
 }
 
-func batterywidget(widget chan<- *BarWidget, device string) {
-	// FIXME: figure out how to not repeat code here
-
+func batteryUpdate(device string) (*BarWidget, int) {
 	capacity, err := ioutil.ReadFile(fmt.Sprintf("/sys/class/power_supply/%s/capacity", device))
 	if err != nil {
 		log.Fatal("Unable to read battery capacity:", err)
@@ -38,87 +36,81 @@ func batterywidget(widget chan<- *BarWidget, device string) {
 		status = "warn"
 	}
 
-	widget <- &BarWidget{
+	return &BarWidget{
 		Name:     "battery",
 		Instance: device,
 		FullText: fmt.Sprintf("%d%%", percent),
 		Status:   status,
-	}
+	}, percent
+}
+
+func batteryWidget(widget chan<- *BarWidget, device string) {
+	w, percent := batteryUpdate(device)
+	widget <- w
 
 	c := time.Tick(15 * time.Second)
 	for range c {
-		capacity, err := ioutil.ReadFile(fmt.Sprintf("/sys/class/power_supply/%s/capacity", device))
-		if err != nil {
-			log.Fatal("Unable to read battery capacity:", err)
-		}
-
 		oldpercent := percent
-
-		// FIXME: see if there is a better way to do this
-		percent := 100
-		fmt.Sscanf(string(capacity), "%d", &percent)
-		if percent > 100 {
-			percent = 100
-		}
+		w, percent := batteryUpdate(device)
 
 		if percent != oldpercent {
-			status := "normal"
-			if percent <= 15 {
-				status = "warn"
-			}
-
-			widget <- &BarWidget{
-				Name:     "battery",
-				Instance: device,
-				FullText: fmt.Sprintf("%d%%", percent),
-				Status:   status,
-			}
+			widget <- w
 		}
 	}
 }
 
-func clockwidget(widget chan<- *BarWidget) {
-	// FIXME: figure out how to not repeat code here
-	now := time.Now()
-	widget <- &BarWidget{
+func clockUpdate(now time.Time) *BarWidget {
+	return &BarWidget{
 		Name:      "clock",
 		FullText:  now.Format("Mon Jan 2 15:04"),
 		ShortText: now.Format("15:04"),
 	}
+}
+
+func clockWidget(widget chan<- *BarWidget) {
+	now := time.Now()
+	widget <- clockUpdate(now)
 
 	// sleep until the next minute
 	duration := now.Add(1 * time.Minute).Truncate(time.Minute).Sub(now)
 	time.Sleep(duration)
 
-	// FIXME: figure out how to not repeat code here
-	now = time.Now()
-	widget <- &BarWidget{
-		Name:      "clock",
-		FullText:  now.Format("Mon Jan 2 15:04"),
-		ShortText: now.Format("15:04"),
-	}
+	widget <- clockUpdate(time.Now())
 
 	c := time.Tick(1 * time.Minute)
 	for now := range c {
-		widget <- &BarWidget{
-			Name:      "clock",
-			FullText:  now.Format("Mon Jan 2 15:04"),
-			ShortText: now.Format("15:04"),
-		}
+		widget <- clockUpdate(now)
 	}
+}
+
+// TODO: make this generic
+func themeBattery(battery *BarWidget) {
+	battery.FullText = fmt.Sprintf("\uf3cf %s", battery.FullText)
+	if battery.Status == "warn" {
+		battery.Color = "#e37170"
+	} else {
+		battery.Color = "#7f9f7f"
+	}
+}
+
+// TODO: make this generic
+func themeClock(clock *BarWidget) {
+	clock.FullText = fmt.Sprintf("\uf017 %s", clock.FullText)
 }
 
 func main() {
 	fmt.Print(`{"version":1}[[]`)
 
 	batw := make(chan *BarWidget)
-	go batterywidget(batw, "BAT0")
+	go batteryWidget(batw, "BAT0")
 
 	clockw := make(chan *BarWidget)
-	go clockwidget(clockw)
+	go clockWidget(clockw)
 
 	battery := <-batw
+	themeBattery(battery)
 	clock := <-clockw
+	themeClock(clock)
 	widgets := []*BarWidget{battery, clock}
 
 	for {
@@ -134,9 +126,11 @@ func main() {
 		select {
 		case msg := <-batw:
 			battery = msg
+			themeBattery(battery)
 			widgets = []*BarWidget{battery, clock}
 		case msg := <-clockw:
 			clock = msg
+			themeClock(clock)
 			widgets = []*BarWidget{battery, clock}
 		}
 	}
