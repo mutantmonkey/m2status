c371e96f63db6b6484ae890e4f902a529cf053fb
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-09-25 00:04:17 -0700

    add wifi widget

--- bar.go
+++ bar.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"time"
 	"strconv"
 	"strings"
+	"syscall"
+	"time"
+	"unsafe"
 )
 
 type BarWidget struct {
@@ -83,9 +85,78 @@ func clockWidget(widget chan<- *BarWidget) {
 	}
 }
 
+func wifiUpdate(iface string) (*BarWidget, string) {
+	const (
+		// these values come from linux/wireless.h (V22)
+		IFNAMSIZ          = 16
+		SIOCGIWESSID      = 0x8B1B
+		IW_ESSID_MAX_SIZE = 32
+	)
+
+	// create a buffer for the interface name
+	ifaceBuf := [IFNAMSIZ]byte{}
+	copy(ifaceBuf[:], iface[:])
+
+	// create an empty buffer for the ESSID (the kernel fills this in)
+	essidBuf := [IW_ESSID_MAX_SIZE]byte{}
+
+	type essidReq struct {
+		Interface [IFNAMSIZ]byte
+		Pointer   *[IW_ESSID_MAX_SIZE]byte
+		Length    uint16
+		Flags     uint16
+	}
+
+	req := &essidReq{
+		Interface: ifaceBuf,
+		Pointer:   &essidBuf,
+		Length:    IW_ESSID_MAX_SIZE,
+		Flags:     0,
+	}
+
+	sock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_DGRAM, 0)
+	if err != nil {
+		log.Fatal("Unable to get socket:", err)
+	}
+
+	_, _, serr := syscall.Syscall(syscall.SYS_IOCTL, uintptr(sock), uintptr(SIOCGIWESSID), uintptr(unsafe.Pointer(req)))
+	if serr != 0 {
+		log.Fatal("Syscall failed:", err)
+	}
+
+	essid := string(essidBuf[:req.Length])
+
+	return &BarWidget{
+		Name:     "wifi",
+		Instance: iface,
+		FullText: essid,
+	}, essid
+}
+
+func wifiWidget(widget chan<- *BarWidget, iface string) {
+	w, essid := wifiUpdate(iface)
+	widget <- w
+
+	c := time.Tick(15 * time.Second)
+	for range c {
+		oldessid := essid
+		w, essid := wifiUpdate(iface)
+
+		if essid != oldessid {
+			widget <- w
+		}
+	}
+}
+
+// TODO: make this generic
+func themeWifi(wifi *BarWidget) {
+	wifi.Color = "#dfaf8f"
+	wifi.FullText = fmt.Sprintf("\uf405  %s", wifi.FullText)
+}
+
 // TODO: make this generic
 func themeBattery(battery *BarWidget) {
-	battery.FullText = fmt.Sprintf("\uf3cf %s", battery.FullText)
+	battery.FullText = fmt.Sprintf("\uf3cf  %s", battery.FullText)
 	if battery.Status == "warn" {
 		battery.Color = "#e37170"
 	} else {
@@ -95,11 +166,14 @@ func themeBattery(battery *BarWidget) {
 
 // TODO: make this generic
 func themeClock(clock *BarWidget) {
-	clock.FullText = fmt.Sprintf("\uf017 %s", clock.FullText)
+	clock.FullText = fmt.Sprintf("\uf017  %s", clock.FullText)
 }
 
 func main() {
 	fmt.Print(`{"version":1}[[]`)
+
+	wifiw := make(chan *BarWidget)
+	go wifiWidget(wifiw, "wlp3s0")
 
 	batw := make(chan *BarWidget)
 	go batteryWidget(batw, "BAT0")
@@ -107,11 +181,13 @@ func main() {
 	clockw := make(chan *BarWidget)
 	go clockWidget(clockw)
 
+	wifi := <-wifiw
+	themeWifi(wifi)
 	battery := <-batw
 	themeBattery(battery)
 	clock := <-clockw
 	themeClock(clock)
-	widgets := []*BarWidget{battery, clock}
+	widgets := []*BarWidget{wifi, battery, clock}
 
 	for {
 		// TODO: handle icon and _status
@@ -124,14 +200,18 @@ func main() {
 		fmt.Print("," + string(b))
 
 		select {
+		case msg := <-wifiw:
+			wifi = msg
+			themeWifi(wifi)
+			widgets = []*BarWidget{wifi, battery, clock}
 		case msg := <-batw:
 			battery = msg
 			themeBattery(battery)
-			widgets = []*BarWidget{battery, clock}
+			widgets = []*BarWidget{wifi, battery, clock}
 		case msg := <-clockw:
 			clock = msg
 			themeClock(clock)
-			widgets = []*BarWidget{battery, clock}
+			widgets = []*BarWidget{wifi, battery, clock}
 		}
 	}
 
